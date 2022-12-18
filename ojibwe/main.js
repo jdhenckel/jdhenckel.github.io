a56c4ba6c7d935e776db1284df2349d6e9859455
John Henckel <jdhenckel@gmail.com>
2022-12-17 23:03:19 -0600

    try syllabary

--- ojibwe/main.js
+++ ojibwe/main.js
@@ -29,9 +29,46 @@ For example,
 
 let stories = null;
 let currentStoryName = null;
+let currentMode = 9;
 const sep_tag = '∴';  // any bizzare characters can work here
 const sep_tr = '∵';
 const sep_pg = '^P';
+
+const vowels = ['e','i','o','a','ii','oo','aa'];
+const syllabary = [
+    "       ᐁ ᐃ ᐅ ᐊ ᐄ ᐆ ᐋ ᐞ ",
+    " p b   ᐯ ᐱ ᐳ ᐸ ᐲ ᐴ ᐹ ᑉ ",
+    " t d   ᑌ ᑎ ᑐ ᑕ ᑏ ᑑ ᑖ ᑦ ",
+    " k g   ᑫ ᑭ ᑯ ᑲ ᑮ ᑰ ᑳ ᒃ ",
+    " ch j  ᒉ ᒋ ᒍ ᒐ ᒌ ᒎ ᒑ ᒡ ",
+    " m     ᒣ ᒥ ᒧ ᒪ ᒦ ᒨ ᒫ ᒻ ",
+    " n     ᓀ ᓂ ᓄ ᓇ ᓃ ᓅ ᓈ ᓐ ",
+    " s z   ᓭ ᓯ ᓱ ᓴ ᓰ ᓲ ᓵ ᔅ ",
+    " sh zh ᔐ ᔑ ᔓ ᔕ ᔒ ᔔ ᔖ ᔥ ",
+    " y     ᔦ ᔨ ᔪ ᔭ ᔩ ᔫ ᔮ ᔾ ",
+    " w     ᐌ ᐎ ᐒ ᐗ ᐐ ᐔ ᐙ ᐤ ",
+];
+
+function convertToSyllabary(w) {
+    // Convert Ojibwe to Syllabary
+    w="ningoding idash oshkinaweg gii-ikidoowag Daga makadekedaa wii-waabandandaa aaniin ezhiwebadogwe i'imaa noondaagwak mii dash gii-makadekewaad animikii-wikwedong gii-onji-maajiitaawag gaawiikaa gii-wiisinisiiwag biinish ginwezh nishwaasagonagak idash gii-aamajiwewag imaa waajiwing eshkam apiji enigok noondaagoziwan animikiin ningoding idash dibishkoo gegoo baakaakonigaadeg mii iw ge-ini'kaanig aanakwad mii dash imaa";
+    w=w.toLowerCase();
+    let a=w.split(/(p|b|t|d|k|g|ch|j|m|n|sh|s|zh|z|y|w)?(aa|ii|oo|a|e|i|o)?/);
+    let result = '';
+    for (let i=0; i+2 < a.length; i+=3) {
+        result += a[i]=='h'||a[i]=='H' ? 'ᐦ' : a[i];
+        let u = a[i+1] ? syllabary.findIndex(s => s.indexOf(a[i+1]) > 0) : 0;
+        let v = a[i+2] ? vowels.indexOf(a[i+2])*2 + 7 : 21;
+        //console.log(1,a[i+1],1,u,v);
+        result += String.fromCharCode(syllabary[u].charCodeAt(v));// + `[${u},${(v-7)/2}]`;
+        // todo skip (0,21)
+        if (u==0 && v==21) result += '@@@';
+    }
+    console.log(result);
+    get('story').innerHTML = result;
+    return result;
+}
+convertToSyllabary();
 
 function newWord(word) {
     // input "ojibwe ∵ english" and output pair [ojibwe, english]
@@ -41,11 +78,12 @@ function newWord(word) {
 }
 
 function loadStory(s) {
+    const new_pg = sep_tag + sep_pg + sep_tag;
     return axios.get(s+'.html').then(res => {
         let text = res.data;
         text = text.substr(text.indexOf('<body'));
         text = text.replaceAll(/\s+/g,' ');
-        text = text.replaceAll(/<br\/?>/ig,sep_tag + sep_pg);
+        text = text.replaceAll(/<br\/?>/ig,new_pg);
         text = text.replaceAll(/<\/b>/ig,sep_tr);
         text = text.replaceAll(/<b>/ig,sep_tag);
         text = text.replaceAll(/<.*?>/g,'');
@@ -100,10 +138,40 @@ function renderPageList() {
 }
 
 function renderStory(words) {
-    result = '';
+    let result = '<p>';
+    let gap = true;
+    if (currentMode == 0) {
+        // Mode 0 is special, it shows Ojibwe with English hover text
+        for (let w of words) {
+            if (w[0]==sep_pg) {
+                result += '</p>\n<p>';
+                continue;
+            }
+            if (gap) result += ' ';
+            result += `<span title="${w[1]}">${w[0]}</span>`;
+            gap = !w[0].endsWith('-');
+        }
+        return result + '</p>';
+    }    
+    // Mode is three BITS, 1=Ojibwe, 2=Syllabary, 4=English
+    // and any combination by adding them together.
+
     for (let w of words) {
-        if (w[0]==sep_pg) result += '<p>';
-        else result += ' ' + w[0];
+        if (w[0]==sep_pg) {
+            result += '</p>\n<p>';
+            continue;
+        }
+        //let s = lookupSyllabary(w);
+        if ((currentMode & 1) == 1) {
+     result += ' ' + w[0];
+            
+        }
+        if ((currentMode & 2) == 2) {
+            
+        }
+        if ((currentMode & 4) == 4) {
+            
+        }
     }
     return result;
 }
@@ -129,14 +197,19 @@ function viewPageList() {
 
 function viewStory(name) {
     storyMode(true);
-    currentStoryName = name;
-    get('story').innerHTML = renderStory(stories[name]);
+    if (name) currentStoryName = name;
+    get('story').innerHTML = renderStory(stories[currentStoryName]);
 }
 
 
-
 function viewMode(m) {
-
+    if (m == 'o') currentMode ^= 1;
+    if (m == 's') currentMode ^= 2;
+    if (m == 'e') currentMode ^= 4;
+    if (m == 'h') currentMode ^= 8;
+    get('mode').innerHTML = currentMode;
+    get('hover').classList.toggle('dim', currentMode < 8);
+    viewStory();
 }
 
 function onLoad() {
